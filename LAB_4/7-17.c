3bc186e0845c8529cb3222f1aae11bd7dda76b6d
Salman <80669080+salman3758@users.noreply.github.com>
2021-04-19 23:10:23 +0500

    Add files via upload

---
+++ LAB_4/7-17.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+double x, y, z, pi;
+int i, count, points; 
+pthread_mutex_t lock= PTHREAD_MUTEX_INITIALIZER;
+
+void *worker_thread(void* args)
+{
+      x = (double)rand()/RAND_MAX;
+      y = (double)rand()/RAND_MAX;
+      z = x*x + y*y;
+      
+      if (z <= 1) 
+      { 
+      	count++;
+      } 
+     pthread_exit(0);
+}
+
+
+int main()
+{
+	printf("Enter number of points to be generated:  ");
+	scanf("%d",&points);
+	
+	pthread_t tid[points];
+	  
+     	for ( i = 0; i <= points; i++) 
+     	{
+		pthread_mutex_lock(&lock);	
+	   	pthread_create(&tid[i], NULL, worker_thread, NULL);
+	   	pthread_join(tid[i], NULL);
+	   	pthread_mutex_unlock(&lock);
+   	}
+ 
+	pi = (double)4*count/(double)points;  
+	
+	printf("Estimated value of pi = %lf \n", pi); 	   	
+   	
+   	return 0;
+}
+
